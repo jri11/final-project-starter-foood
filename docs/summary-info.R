46b9999b0d668ce180d6b494af378b065e85d15a
brendanwilliam <brendanwillkeane@gmail.com>
2022-02-15 12:58:23 -0800

    added datasets and files to our project for P02

---
+++ docs/summary-info.R
@@ -0,0 +1,10 @@
+# Date: February 15th, 2022
+# Author: Brendan Keane, Jerry Zhang
+# Purpose: Summarize information from the COVID-19 Healthy Diet Dataset
+
+# Loading global covid data
+global_covid_data <- read.csv("./data/jhu-covid19-2022-02-14.txt")
+
+# Loading global food supply in kg
+global_food_supply_kg <- read.csv("./data/Food_Supply_Quantity_kg_Data.csv")
+
