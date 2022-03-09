80bebcb5d67294bc1ef7cc4d87b7e8794214dace
Jessy <jri11@uw.edu>
2022-03-09 10:30:57 -0800

    Vis 1 explanation done

--- source/tab_vis1.R
+++ source/tab_vis1.R
@@ -10,21 +10,14 @@ vis1_tab <- tabPanel(
   "Visualization 1",
   plotOutput("lethal_foods"),
   h1("Interactive Page 1"),
-  p("Creates a well laid out page with a set of 1+ controls that configure a 
-    chart. Chart must be intentionally designed to reveal particular patterns in 
-    the data, and meet course standards of labeling // clarity."),
-  p("(2pts) A description of what the chart attempts to answer/understand"),
-  p("(4pts) Selected the appropriate chart type / graphical encoding based on 
-    the question of interest and the data type(s) of the features"),
-  p("(4pts) Appropriate labels/legends, depending on your chart type. Including, 
-  but not limited to: - X axis label (if there's an x axis) - Y axis label (if 
-  there's an y axis) - Title (if there's an ability to add a title -- e.g., not 
-  a map) - Legend for any encodings that are not immediately apparent"),
-  p("(2pts) Provides well-formatted and detailed information when hovering each 
-    mark on the chart"),
-  p("(2pts) for creating at least 1 widget with appropriate label"),
-  p("(1pt) for setting appropriate default value"),
-  p("(3pts) for the widget working (e.g., changing the desired output)"),
-  p("(2pts) for appropriate layout of controls and visualization (e.g., a 
-    sidebarLayout())")
+  h4("Alcohol is generally to be consumed in moderation, and copious amount is
+     not healthy for anyone, however since alcohol has this unhealthy reputation,
+     it would be easy to assume that people who consume more alcohol are more
+     susceptible to COVID-19 exposure. Contradictaory to this misconception,
+     the data we explored, shows no clear pattern of alcohol consumption and fatality.
+     In addition, It appears as though even if the alcoholic beverage consumption
+     is over fifteen, there is no correleation with the fatality ratio. This
+     does not mean that alcohol actually helps with the immune system avioding 
+     COVID, but it does not have any clear data/patterns that it hurts either.")
+
 )
