c257842a57d6c125307a1f1317ca10c0f91ce031
”Jerry020git config --global user.email “jzhang43@uw.edu <jibson@fudabao-MacBook-Pro.local>
2022-03-09 01:22:04 -0800

    Jerry last push

--- source/tab_vis3.R
+++ source/tab_vis3.R
@@ -31,6 +31,6 @@ vis3_tab <- tabPanel(
     )
   ),
   widths = c(2,12),
-  p("The purpose of this chart is to explore the relationship between the number of cases and deaths across countries and to derive their ratios. In addition, another aspect is to calculate the ratio between them and explore the relationship between the ratio and the ratio of nutritious food intake.")
+  p("The purpose of this chart is to explore the relationship between the number of cases and deaths across countries and to derive their ratios. In addition, another aspect is to calculate the ratio between them and explore the relationship between the ratio and the ratio of nutritious food intake. I consider that the intake of food nutrition is related to immunity, which in turn will affect people's resistance to COVID, so I want to explore the relationship between COVID cases and deaths and food nutrition intake, so I am doing both Before the relationship, I first choose to show the number of cases and deaths of COVID in each country to find the pattern.")
 )
 
